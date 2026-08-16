fcf179f02f08fccf7121337942c8dabb712d5cf6
ljxx11 <274144101+ljxx11@users.noreply.github.com>
2026-08-16 17:56:57 -0400

    Refresh cache for Ledger v3.1.0

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='ledger-v3.0.0-monthly-budget.11';
+const CACHE='ledger-v3.1.0-card-refunds.1';
 const CORE=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',event=>{
